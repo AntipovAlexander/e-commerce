10f6e1057d7fb73d436c097f158d9348e1ddb017
Oleksandr <antipov@themindstudios.com>
2025-04-16 13:19:38 +0300

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "e-commerce"
+include(":app")
+include(":features:home")
+include(":features:browse")
+include(":features:favourites")
+include(":features:cart")
+include(":features:profile")
+include(":features:sign_up")
+include(":features:product_details")
+include(":core:theme")
