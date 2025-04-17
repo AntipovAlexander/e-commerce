41f5f4fc2529b66614b1a98d3ec6ae3e10095345
Oleksandr <antipov@themindstudios.com>
2025-04-17 16:00:15 +0300

    optimized dependencies

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -8,14 +8,11 @@ android {
 }
 
 dependencies {
-
+    // todo: delete this module?
     implementation(libs.kotlin.serialization.json)
     implementation(libs.androidx.navigation)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
--- features/onboarding/build.gradle.kts
+++ features/onboarding/build.gradle.kts
@@ -13,10 +13,4 @@ dependencies {
     implementation(project(":features:onboarding:forgot_password"))
     implementation(libs.androidx.navigation)
     implementation(libs.kotlin.serialization.json)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
